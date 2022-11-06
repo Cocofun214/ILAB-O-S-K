9094f534a95394089d3d7fae004975ff89435b28
jonnn27 <108605984+Cocofun214@users.noreply.github.com>
2022-11-06 20:06:47 +0700

    Update TOPIK-2.asm

--- TOPIK-2.asm
+++ TOPIK-2.asm
@@ -1,9 +1,42 @@
-   ; directive include
+No [1]
+
+; directive include
 %include "asm_io.inc"
 
 segment .data
    ; directive Dx
 	hello   db 'Hello,World!', 0
+segment .bss
+   ; directive RESx
+
+segment .text
+   global _main
+   _main:
+      ; Routine “setup”
+      enter  0, 0
+      pusha
+
+      ; Program Anda di bawah
+	 mov     eax, hello
+	 call    print_string
+
+      ; Routine “cleanup”
+      popa
+      mov    eax, 0
+      leave
+      ret
+
+
+
+No [2]
+
+
+   ; directive include
+%include "asm_io.inc"
+
+segment .data
+   ; directive Dx
+	hello   db 'Selamat Datang di Universitas Gunadarma', 0
 segment .bss
    ; directive RESx
 
