824cca872e0b06c3928f1d44076f68c1ef3de055
-----J---- <108605984+Cocofun214@users.noreply.github.com>
2022-11-14 23:49:41 +0700

    Create TOPIK-5.asm

---
+++ TOPIK-5.asm
@@ -0,0 +1,114 @@
+================= Prelab 5 ==============
+
+--- No.1 ----
+
+Intruksi CMP
+
+
+--- No.2 ---
+
+// mov ax, 5
+
+JG
+
+--- No.3 ----
+
+// Intruksi JO
+
+OF = 1
+
+
+--- No.4 ----
+
+// mov ax, -42
+
+tidak
+
+
+--- No.5 ----
+
+// percabangan unconditional
+
+JMP
+
+
+--- No.6 ----
+
+// SF = OF dalam a>b (a-b>0) jiika
+
+* Jika tidak ada overflow, maka (a-b) adalah bil.positif (yang valid)
+* Jika ada overflow, maka (a-b) adalah bil.negatif (yang tidak valid)
+
+
+--- No.7 ----
+
+// Intruksi JNZ
+
+ZF = 0
+
+
+--- No.8 ----
+
+// CMP a,b
+
+ZF = 0 dan CF = 1
+
+
+--- No.1/2 ----
+
+// Intruksi JMP yang benar
+
+* Intruksi JMP hanya bisa lompat ke label dalam prosedur yang dikerjakan
+* Intruksi JMP adalah unconditional tranfer instruction
+
+
+--- No.2/2 ----
+
+// alur instruksi
+
+* Uncontional
+* Conditional
+
+
+--- No.3/2 ----
+
+// Intruksi JNA
+
+JBE
+
+
+--- No.4/2 ----
+
+// mengenai instruksi CMP
+
+instruksi CMP membandingkan nilai dari dua operand dan men-set register EFLAG berdasarkan hasilnya
+
+
+--- No.5/2 ----
+
+// Flag-Flag
+
+* ZF
+* OF
+* SF
+
+--- No.6/2 ----
+
+//instruksi JNG
+
+JLE
+
+
+--- No.7/2 ----
+
+// JMP digunakan untuk..
+
+Lompat ke suatu llabel kode
+
+
+--- No.8/2 ----
+
+// 2 instruksi
+
+* Instruksi operand
+* instruksi lompat kondisional
